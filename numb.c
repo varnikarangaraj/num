0537cb1ff80d2feb149cb4b5af11b23a69a2352e
varnikarangaraj <varni014@gmail.com>
2016-07-10 19:29:10 +0530

    Create numb.c

---
+++ numb.c
@@ -0,0 +1,25 @@
+ 
+#include <stdio.h>
+ 
+int main()
+{
+    int n;
+    printf("Enter any number: ");
+    scanf("%d", &n);
+     if(n > 0)
+    {
+        printf("Number is POSITIVE");
+    }
+    else if(num < 0)
+    {
+        printf("Number is NEGATIVE");
+    }
+    else
+    {
+        printf("Number is ZERO");
+    }
+ 
+    return 0;
+} 
+
+
